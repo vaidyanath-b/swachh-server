8427d30f06e2f4bec5954d017ba912acd2b1d07c
vaidyanath b <b.vaidyanath003@gmail.com>
2022-12-02 20:18:16 +0530

    report synced

--- src/routes/collection/serveRequest.js
+++ src/routes/collection/serveRequest.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const CollectionReport = requrie("../../models/Collection")
+const CollectionReport = require("../../models/Collection")
 
 router.use('/', (req, res) => {
     try {
