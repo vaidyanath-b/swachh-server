dfd42aac313807627dc90d5bff8f74f3c826ae23
vaidyanath b <b.vaidyanath003@gmail.com>
2022-12-07 00:22:10 +0530

    authentication

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,23 +1,51 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 const Collector = require('../models/Collector')
 const login = async (req, res) => {
     try {
-        const { username, password } = req.body;
-        const user = await Collector.findOne({ "username": username });
-        console.log(user.username);
-        if (!user || !password)
-            return res.status(401).json({ error: "invalid username and password" });
-        if (user.password == password) {
-            res.status(200).json({
-                message: "Logged in",
-                username: user.username
-
-            })
+        if (!req.body.username || !req.body.password) {
+            return res.status(400).json({ error: "Please enter username and password" })
         }
-        else return res.status(401).json({
-            message: "invalid password"
-        })
+
+        Collector.findOne({ "username": req.body.username })
+
+            .exec(err, user)
+        {
+            if (err) {
+                return res.status(402).json({ error: err })
+            }
+            if (!user) {
+                return res.status(403).json({ error: "User not found" })
+            }
+            var isPasswordValid = bcrypt.compareSync(user.password, req.body.password)
+
+            if (!isPasswordValid)
+                return res.status(401).json({
+                    accessToken: null,
+                    message: "incorrect password"
+                });
+            var token = jwt.sign({
+                id: user.id
+            },
+                process.env.API_SECRET,
+                {
+                    expiresIn: 86400
+                }
+            )
+            res.status(200).json({
+                user: {
+                    id: user._id,
+                    username: user.usernam
+                },
+                message: "login succesful",
+                accessToken: token
+            });
+
+
+        }
+
     }
     catch (err) {
         return res.status(401).json({ error: err })
@@ -26,22 +54,26 @@ const login = async (req, res) => {
 
 const signup = async (req, res) => {
     try {
-        const { username, password, location } = req.body;
-        if (!username)
-            res.status(401).json({ error: "invalid username" })
-        if (!password)
-            res.status(401).json({ error: "invalid pasword" })
-        if (!location)
-            res.status(401).json({ error: "invalid location" })
 
-        const user = await Collector.create({
-            username, password, location
+
+        const user = await new User({
+            username: req.body.username,
+            password: bcrypt.hashSync(req.body.password, 8),
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
         })
-        res.status(200).json({
-            message: "account created",
-            username: user.username
+        user.save((err, user) => {
+            if (err)
+                return res.status(401).json({
+                    error: "something went wrong"
+                })
+            return res.status(200).json({
+                message: "signin successfull",
+            })
+
 
         })
+
     }
     catch (err) {
         return res.status(500)
